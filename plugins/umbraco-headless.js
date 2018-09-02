0b8ba64b1e29c813f553e599858c7b61322111d4
mattbrailsford <me@mattbrailsford.com>
2018-09-02 08:50:53 +0100

    Added comment about authentication

--- plugins/umbraco-headless.js
+++ plugins/umbraco-headless.js
@@ -3,8 +3,11 @@ import umbracoConfig from '../umbraco.config';
 
 export default (context, inject) => {
     let umbraco = new UmbracoHeadless.HeadlessService(umbracoConfig);
+    // Umbraco headless doesn't actually need authenticating
+    // for public read requests, but the API doesn't currently
+    // support unauthenticated requests, so to bypass the auth
+    // requests, just make it think it's already authenticated
     umbraco.IsAuthenticated = true;
     umbraco.BaseUrl = umbracoConfig.url;
-    //await umbraco.authenticate()
     inject('umbraco', umbraco)
 }
